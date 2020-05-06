85d4631801c902eef712c82551655ed998e62907
maratkhambelov <maratkhambelov@gmail.com>
2020-05-06 20:01:44 +0300

    Updates

---
+++ precache-manifest.1fc21af8c3659f2dea4c1b14687a1c9f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39419e2c95fea1bba0eb7edb3af77b7c",
+    "url": "/mircod_service-home-page/index.html"
+  },
+  {
+    "revision": "b4d38de87264e23787c8",
+    "url": "/mircod_service-home-page/static/css/2.508070a3.chunk.css"
+  },
+  {
+    "revision": "f4f54ee80f53039ce034",
+    "url": "/mircod_service-home-page/static/css/main.355cce67.chunk.css"
+  },
+  {
+    "revision": "b4d38de87264e23787c8",
+    "url": "/mircod_service-home-page/static/js/2.8d28968e.chunk.js"
+  },
+  {
+    "revision": "a77d181038c246f96f2f789837e5a833",
+    "url": "/mircod_service-home-page/static/js/2.8d28968e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f54ee80f53039ce034",
+    "url": "/mircod_service-home-page/static/js/main.3c0316e8.chunk.js"
+  },
+  {
+    "revision": "71ccaf6c76281c91886d",
+    "url": "/mircod_service-home-page/static/js/runtime-main.3d1fdce5.js"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/mircod_service-home-page/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/mircod_service-home-page/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/mircod_service-home-page/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/mircod_service-home-page/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "3a6d62770530ba2efbf70ab2397f09d9",
+    "url": "/mircod_service-home-page/static/media/facebook.3a6d6277.svg"
+  },
+  {
+    "revision": "379c0307e4f13d3973eb16899456183d",
+    "url": "/mircod_service-home-page/static/media/header-logo.379c0307.png"
+  },
+  {
+    "revision": "03fcc2b689dbbde1994eebd1ea7de296",
+    "url": "/mircod_service-home-page/static/media/latoregular.03fcc2b6.ttf"
+  },
+  {
+    "revision": "1fbf86c9bbb08fd76eb28eb50eeeb86a",
+    "url": "/mircod_service-home-page/static/media/latoregular.1fbf86c9.woff"
+  },
+  {
+    "revision": "fe5fe57c2d0c6109b132693f90747cbe",
+    "url": "/mircod_service-home-page/static/media/mastercard.fe5fe57c.png"
+  },
+  {
+    "revision": "9aeb33805ac692869a8021ed113d0d5b",
+    "url": "/mircod_service-home-page/static/media/micro-scheme.9aeb3380.png"
+  },
+  {
+    "revision": "8b499fcc46b36561025add1988b36436",
+    "url": "/mircod_service-home-page/static/media/product.8b499fcc.png"
+  },
+  {
+    "revision": "ff7614ce20f36cc947eb8a5389f6e44b",
+    "url": "/mircod_service-home-page/static/media/twitter.ff7614ce.svg"
+  },
+  {
+    "revision": "12245447adfa53bf387ac75e5d0dd3c9",
+    "url": "/mircod_service-home-page/static/media/youtube.12245447.svg"
+  },
+  {
+    "revision": "8473f7b6179aab710a1d2f6d0c67f5f1",
+    "url": "/mircod_service-home-page/static/media/zwave_black.8473f7b6.svg"
+  },
+  {
+    "revision": "45660838cccf1c468c85ca3123233bb3",
+    "url": "/mircod_service-home-page/static/media/zwave_white.45660838.svg"
+  }
+]);
